d60f39fbbb24cca9c2a358727485f2a6531dc029
yanickrossi <ya.rossi_nick@hotmail.com>
2020-06-02 20:09:30 -0300

    up

--- ML Study/README.md
+++ ML Study/README.md
@@ -7,23 +7,54 @@
 > - Identify appropriate AWS services to implement ML solutions.
 > - Design and implement scalable, cost-optimized, reliable, and secure ML solutions.
 
+---
+
+#### Domains To study
+
 >1. Domain 1: Data Engineering
 >  - Create data repositories for machine learning.
 >  - Identify and implement a data-ingestion solution.
 >  - Identify and implement a data-transformation solution.
+
 >2. Domain 2: Exploratory Data Analysis
 >  - Sanitize and prepare data for modeling.
 >  - Perform feature engineering.
 >  - Analyze and visualize data for machine learning.
+
 >3. Domain 3: Modeling
 >  - Frame business problems as machine learning problems.
 >  - Select the appropriate model(s) for a given machine learning problem.
 >  - Train machine learning models.
 >  - Perform hyperparameter optimization.
 >  - Evaluate machine learning models.
+
 >4. Domain 4: Machine Learning Implementation and Operations
 >  - Build machine learning solutions for performance, availability, scalability, resiliency, and fault tolerance.
 >  - Recommend and implement the appropriate machine learning services and features for a given problem.
 >  - Apply basic AWS security practices to machine learning solutions.
 >  - Deploy and operationalize machine learning solutions.
 
+
+<h2>Ciclo de Vida - Machine Learning</h2>
+
+- Processamento dos dados
+    - Feature & Labels
+    - Engenharia de Feature
+    - Redução de Feature (Tirar dadps que não fazem sentido)
+    - Encoding -> String para int, por ex.
+    - Formatação -> Formato do arquivo
+- Divisão dos dados
+    - Treinameto -> 80% dos dados
+    - Validação (opcional para validação cruzada)
+    - Teste -> 20% dos dados
+- Treinamento - Parte mais importante do ciclo
+    - Usa os dados para treinar o modelo
+- Teste
+    - Verifica quão bem o modelo se "encaixa" (fit)
+    - Overfit? ***Modelo se ajusta muito bem ao conjunto de dados anteriormente observado, mas se mostra ineficaz para prever novos resultados***
+    - Usa os dados de teste separados na fase de Split, que nunca foram *vistos* antes
+- Deploy 
+    - Onde hospedar o modelo
+    - Utilizar como Batch (lotes) ou como um serviço
+
+- Infer: *Usar o modelo para inferir ou prever resultados*
--- code/agaxtur_query.sql
+++ code/agaxtur_query.sql
@@ -14,7 +14,8 @@ select
     txn_x.x_attrib_52 external_transaction_id,
     ((CASE WHEN txn.TYPE_CD IN ('ACCRUAL') AND txn.SUB_TYPE_CD IN ('Product','Action Based Bonus')AND def_acrl.DISPLAY_NAME IN ('Milhas') THEN acrl.ACCRUALED_VALUE ELSE 0 END) +
     (CASE WHEN txn.TYPE_CD IN ('REDEMPTION') AND txn.SUB_TYPE_CD IN ('Cancellation') AND def_rdm.DISPLAY_NAME in ('Milhas') THEN rdm.VALUE *-1 ELSE 0 END)) as miles_val,
-    tier.name tier
+    tier.name tier,
+    sign.x_attrib_03 as status
 from
 rwzd_siebel_oci.s_loy_txn txn 
 inner join rwzd_siebel_oci.s_prod_int prod on txn.prod_id = prod.row_id
@@ -26,22 +27,29 @@ left join rwzd_siebel_oci.s_loy_acrl_itm acrl on txn.row_id = acrl.txn_id
 left join rwzd_siebel_oci.s_loy_rdm_itm rdm on txn.row_id = rdm.txn_id
 left join rwzd_siebel_oci.S_LOY_ATTRDEFN def_acrl on def_acrl.row_id = acrl.attrib_defn_id
 left join rwzd_siebel_oci.S_LOY_ATTRDEFN def_rdm on def_rdm.row_id = rdm.attrb_defn_id
-left join rwzd_siebel_oci.s_loy_tier tier on member.pr_dom_tier = tier.row_id and tier.created in (
-            select max(tier.created) from rwzd_siebel_oci.s_loy_tier tier 
-            inner join rwzd_siebel_oci.s_loy_member member on member.pr_dom_tier = tier.row_id
-            left join rwzd_siebel_oci.s_loy_txn txn on member.row_id = txn.member_id
-            inner join rwzd_siebel_oci.s_prod_int prod on txn.prod_id = prod.row_id
-            where
-                txn.process_dt > '2020-01-01' and
-                txn.process_dt < '2020-02-01' and
-                prod.name = 'Creditos Agaxtur'and
-                txn.type_cd = 'ACCRUAL'
-            )
+left join rwzd_siebel_oci.s_loy_mem_tier mem_tier on member.row_id = mem_tier.member_id and(
+    txn.process_dt > mem_tier.start_dt
+    and txn.process_dt < mem_tier.end_dt
+)
+left join rwzd_siebel_oci.s_loy_tier tier on tier.row_id = mem_tier.tier_id
+left join rwzd_siebel_oci.cx_sign_member sign on member.row_id = sign.x_attrib_01 
+left join rwzd_siebel_oci.audit_decode audit on sign.row_id = audit.tbl_record_id and(
+    audit.create_dt < process_dt
+ and created_dt in 
+    select min(dt_dif) , created_dt from(
+        select min(process_dt - created_dt) as dt_dif, created_dt
+        from rwzd_siebel_oci.s_loy_txn txn
+        inner join rwzd_siebel_oci.s_loy_member member on member.row_id = txn.member_id
+        inner join rwzd_siebel_oci.cx_sign_member sign on member.row_id = sign.x_attrib_01 
+        inner join rwzd_siebel_oci.audit_decode audit on sign.row_id = audit.tbl_record_id and
+        tbl_name = 'CX_SIGN_MEMBER' and decode_field = 'X_ATTRIB_03'
+    )
+)
 where 
-txn.process_dt > '2020-01-01' and
-txn.process_dt < '2020-02-01' and
-prod.name = 'Creditos Agaxtur'and
-txn.type_cd = 'ACCRUAL'
+    txn.process_dt > '2020-01-01' and
+    txn.process_dt < '2020-02-01' and
+    prod.name = 'Creditos Agaxtur'and
+    txn.type_cd = 'ACCRUAL'
 
 
 
